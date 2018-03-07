02baf7bf98b7a6d3ece68bab9a9bacbf308d5556
xinxin.chen <xinxin.chen@yitu-inc>
2018-03-07 16:15:09 +0800

    init

---
+++ src/main/java/com/wlmiao/EduSysApplication.java
@@ -0,0 +1,12 @@
+package com.wlmiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSysApplication.class, args);
+	}
+}
